b4c3ca02d04eb3b22674c375cfa60067cc69cdee
chenyu <cyu@infervision.com>
2020-03-30 19:14:52 +0800

    [ADD]add vr demo page

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import Vr from './pages/vr/index';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Vr} />
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
---
+++ app/src/pages/vr/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+const Vr = () => {
+  return (
+    <div className="demo">
+      这就是一个demo而已~
+    </div>
+  );
+}
+
+export default Vr;
