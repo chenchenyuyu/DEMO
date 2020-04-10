6150ca1e16016f1e9f956a63d550a37fca7bd6a9
chenyu <cyu@infervision.com>
2020-04-10 15:42:50 +0800

    [ADD]add loader dicom

--- app/src/pages/volume/loaderDicom/index.jsx
+++ app/src/pages/volume/loaderDicom/index.jsx
@@ -1,18 +1,39 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Canvas, extend, useThree, useFrame } from 'react-three-fiber';
 import * as THREE from 'three';
+import * as AMI from 'ami.js';
 
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
 
 extend(TrackballControls);
 
 const Scene = () => {
-  // const [gl, camera] = useThree();
-  // const controls = useRef();
-  // useFrame(state => controls.current.update());
+  const { gl, camera } = useThree();
+  const controls = useRef();
+  useFrame(state => controls.current.update());
+  let suffix = ['36444280', '36444294', '36444308'];
+
+  let files = suffix.map(
+    v => `https://cdn.rawgit.com/FNNDSC/data/master/dicom/adi_brain/${v}`
+  );
+
+  useEffect(() => {
+    (async () => {
+      // renderer
+      const threeD = document.getElementById('r3d');
+      const renderer = new THREE.WebGLRenderer({
+        antialias: true
+      });
+      renderer.setSize(threeD.offsetWidth, threeD.offsetHeight);
+      renderer.setClearColor(0x673ab7, 1);
+      renderer.setPixelRatio(window.devicePixelRatio);
+      threeD.appendChild(renderer.domElement);
+      const loader = new AMI.VolumeLoader(threeD);
+    })();
+  }, []);
   return (
     <>
-      {/* <trackballControls args={[camera, gl.domElement]} ref={controls} /> */}
+      <trackballControls args={[camera, gl.domElement]} ref={controls} />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
     </>
@@ -22,7 +43,7 @@ const Scene = () => {
 const LoaderDicom = () => {
   return (
     <div id="r3d" style={{ height: '100vh', width: '100%' }}>
-      <Canvas camera={{ position: [0, 0, 300], near: 0.1, far: 1e5 }}>
+      <Canvas camera={{ position: [250, 250, 250], near: 0.1, far: 1e5 }}>
         <Scene />
       </Canvas>
     </div>
