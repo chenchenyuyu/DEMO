10a38afb86de2357f45f5ac20b76a331d295af8d
chenyu <cyu@infervision.com>
2020-04-13 16:26:15 +0800

    [MOD]modify range components

--- app/src/components/range/index.jsx
+++ app/src/components/range/index.jsx
@@ -1,22 +1,21 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo, useEffect } from 'react';
 
 import './index.less';
 
-const Range = ({
-  min = 1,
-  max = 25,
-  step = 1,
-  onChange,
-  labelTop = 'MinIP'
-}) => {
-  const [value, setValue] = useState(6);
+const Range = ({ title = 'MinIP', min = 1, max = 25, step = 1, onChange }) => {
+  const [value, setValue] = useState(1);
   const indicatorRef = useRef(null);
 
-  let marks = [];
-  for (let i = max; i > 0; i -= step) {
-    marks.push(i);
-  }
+  let marks = useMemo(() => {
+    let marksArr = [];
+    for (let i = max; i > 0; i -= step) {
+      marksArr.push(i);
+    }
+    return marksArr;
+  }, [min, max, step]);
+
   const mouseEventHandler = e => {
+    e.stopPropagation();
     const indicatorBarHeight = indicatorRef.current.clientHeight;
     const { top } = e.currentTarget.getBoundingClientRect();
     const calibration = 1.0 / max / 2;
@@ -26,10 +25,12 @@ const Range = ({
       calibration;
     const slice = Math.floor(Math.max(percentage, 0) * max) + min;
     setValue(slice);
+    if (onChange) onChange(slice);
   };
+
   return (
     <div className="range">
-      <div className="range-title">{labelTop}</div>
+      <div className="range-title">{title}</div>
       <div className="range-label">{max}mm</div>
       <div
         ref={indicatorRef}
@@ -37,14 +38,14 @@ const Range = ({
         style={{ position: 'relative' }}
         onMouseDown={mouseEventHandler}
       >
-        {marks.map(i => {
+        {marks.map(index => {
           return (
             <div
-              key={i}
+              key={index}
               style={{
                 width: '8px',
                 height: '1px',
-                background: i < value ? '#3F87F5' : '#fff',
+                background: index < value ? '#3F87F5' : '#fff',
                 marginBottom: '8px',
                 marginLeft: '12px'
               }}
