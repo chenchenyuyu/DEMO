d219c67e1fc1a78c471653405f5ead0083abd6fb
chenyu <cyu@infervision.com>
2020-05-18 11:53:44 +0800

    [MOD]modify tracheaLabelMap json

--- app/src/components/checkboxGroup/data.js
+++ app/src/components/checkboxGroup/data.js
@@ -15,6 +15,41 @@ const CT_LUNG_NODULE_LSDS = {
   }
 };
 
+const tracheaLabelMap = {
+  trachea: 1,
+  R_main_bronchus: 2,
+  R_super_lb_bronchus: 3,
+  R_super_lb_apical_bronchus: 4,
+  R_super_lb_poster_bronchus: 5,
+  R_super_lb_anter_bronchus: 6,
+  R_intermedius_bronchus: 7,
+  R_middle_lb_bronchus: 8,
+  R_middle_lb_lateral_bronchus: 9,
+  R_middle_lb_medial_bronchus: 10,
+  R_infer_lb_bronchus: 11,
+  R_infer_lb_dorsal_bronchus: 12,
+  R_infer_lb_basal_bronchus: 13,
+  R_infer_lb_medial_basal_bronchus: 14,
+  R_infer_lb_anter_basal_bronchus: 15,
+  R_infer_lb_lateral_basal_bronchus: 16,
+  R_infer_lb_poster_basal_bronchus: 17,
+  L_main_bronchus: 18,
+  L_super_lb_bronchus: 19,
+  L_super_lb_super_bronchus: 20,
+  L_super_lb_apicoposter_bronchus: 21,
+  L_super_lb_anter_bronchus: 22,
+  L_super_lb_linguar_bronchus: 23,
+  L_super_lb_super_linguar_bronchus: 24,
+  L_super_lb_infer_lingular_bronchus: 25,
+  L_infer_lb_bronchus: 26,
+  L_infer_lb_dorsal_bronchus: 27,
+  L_infer_lb_basal_bronchus: 28,
+  L_infer_lb_medial_anter_basal_bronchus: 29,
+  L_infer_lb_lateral_basal_bronchus: 30,
+  L_infer_lb_poster_basal_bronchus: 31,
+};
+
+
 const CT_LUNG_DATA = {
   lobes: CT_LUNG_LOBES,
   lsds: CT_LUNG_NODULE_LSDS,
