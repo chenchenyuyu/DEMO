ddd040bede9c02a480fd88b5d145fca6eb8b39af
chenyu <cyu@infervision.com>
2020-05-28 19:57:38 +0800

    [ADD]add grayImage screenShot

--- app/src/pages/vr/vtk/index.jsx
+++ app/src/pages/vr/vtk/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useMemo, Suspense } from 'react';
+import React, { useRef, useMemo, Suspense, useState, useEffect } from 'react';
 import {
   Canvas,
   useFrame,
@@ -19,14 +19,14 @@ import './index.less';
 
 extend(TrackballControls);
 
-const VtkLoader = ({ url }) => {
+const VtkLoader = ({ url, getScreenShot }) => {
   const vtk = useLoader(VTKLoader, url);
   useMemo(() => {
     vtk.computeVertexNormals(); // 光滑数据
     vtk.translate(0, 0, 0);
   }, [vtk]);
   return (
-    <mesh geometry={vtk}>
+    <mesh geometry={vtk} onPointerMove={getScreenShot}>
       {/* <bufferGeometry attach="geometry"/> */}
       <meshPhongMaterial
         attach="material"
@@ -43,7 +43,7 @@ const VtkLoader = ({ url }) => {
   );
 };
 
-const Scene = ({ model, position }) => {
+const Scene = ({ model, position, getScreenShot }) => {
   const paramRef = useRef();
   useMemo(() => {
     if (Object.values(model).length > 0) {
@@ -61,7 +61,7 @@ const Scene = ({ model, position }) => {
       >
         <Suspense fallback={null}>
           {paramRef.current.map(url => (
-            <VtkLoader url={url} key={url} />
+            <VtkLoader url={url} key={url} getScreenShot={getScreenShot}/>
           ))}
         </Suspense>
       </group>
@@ -88,6 +88,14 @@ const Lights = () => {
 };
 
 const Vtk = () => {
+  const [ threeUrl, setThreeUrl ] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      getScreenShot();
+    }, 3000)
+  }, []);
+
   const handleScreenShot = () => {
     const canvas = document.querySelector('.canvas-element canvas');
     const imageUrl = canvas.toDataURL('image/jpeg', 0.3);
@@ -96,7 +104,7 @@ const Vtk = () => {
 
   const getScreenShot = () => {
     // 0. create canvas
-    const threeElement = document.querySelector('.three-element canvas');
+    const threeElement = document.querySelector('.canvas-element canvas');
     const sourceCanvas = document.createElement('canvas');
     const sourceCtx = sourceCanvas.getContext('2d');
     const destinationCanvas = document.createElement('canvas');
@@ -129,7 +137,7 @@ const Vtk = () => {
     destinationCtx.restore();
     // 4. output imageUrl
     const url = destinationCanvas.toDataURL('image/png', 1);
-    // setImageUrl(url);
+    setThreeUrl(url);
   };
 
   const getGrayscaleImage = (pixelsData) => {
@@ -144,7 +152,11 @@ const Vtk = () => {
 
   return (
     <div style={{ height: '100vh', width: '100%' }}>
-      <div style={{textAlign: 'center'}}><Button onClick={handleScreenShot}>截图啊</Button></div>
+      <div style={{textAlign: 'center'}}>
+        <Button onClick={handleScreenShot}>截图</Button>
+        <Button onClick={getScreenShot}>灰度图</Button>
+      </div>
+      <img src={threeUrl} alt="gray" style={{position: 'absolute', top: '0px', left: '0px', width: '570px', height: '400px'}}/>
       <Canvas
         className="canvas-element"
         orthographic={true}
@@ -160,12 +172,17 @@ const Vtk = () => {
         }}
         gl={{ preserveDrawingBuffer: true }}
       >
-        <Scene model={lsdsModel} position={[
+        <Scene 
+          model={lsdsModel}
+          getScreenShot={getScreenShot}
+          position={[
           -150.36083055745286,
           -120.26194966636896,
           -148.30930405085903
         ]}/>
-        <Scene model={tracheaModel} position={[
+        <Scene
+          model={tracheaModel}
+          position={[
           -191.36083055745286,
           -195.26194966636896,
           -159.30930405085903
