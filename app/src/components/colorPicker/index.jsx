25bc954ec18bffc34b859633c353fb866385f032
chenyu <cyu@infervision.com>
2020-08-11 14:39:20 +0800

    [ADD]add colorpicker demo

--- app/src/components/colorPicker/index.jsx
+++ app/src/components/colorPicker/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SketchPicker } from 'react-color'
 
 import './index.less';
 
 
 const ColorPicker = () => {
+  const [ color, setColor ] = useState('');
   return (
     <div className="color-picker">
-      ColorPicker
+      <SketchPicker />
     </div>
   );
 };
