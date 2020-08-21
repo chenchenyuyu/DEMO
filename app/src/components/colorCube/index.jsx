c82478e4b39b574c38d87568ce42573ff281c90f
chenyu <cyu@infervision.com>
2020-08-21 16:26:15 +0800

    [MOD]modify colorCube component

--- app/src/components/colorCube/index.jsx
+++ app/src/components/colorCube/index.jsx
@@ -5,16 +5,16 @@ import './index.less';
 const ColorCube = ({ color, onExpand, expand }) => {
   return(
     <div className="color-cube" onClick={() => onExpand(!expand)}>
-      <ul class="color-cube-inner">
-        <li class="top" style={{background: color}}></li>
-        <li class="bottom" style={{background: color}}></li>
-        <li class="front" style={{background: color}}></li>
-        <li class="back" style={{background: color}}></li>
-        <li class="right" style={{background: color}}></li>
-        <li class="left" style={{background: color}}></li>
+      <ul className="color-cube-inner">
+        <li className="top" style={{background: color}}></li>
+        <li className="bottom" style={{background: color}}></li>
+        <li className="front" style={{background: color}}></li>
+        <li className="back" style={{background: color}}></li>
+        <li className="right" style={{background: color}}></li>
+        <li className="left" style={{background: color}}></li>
       </ul>
     </div>
-  )
+  );
 };
 
 export default ColorCube;
